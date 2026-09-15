f8a3ed10bca9beec8f758a2d3bfb73caafb67a9d
Zoeb Mithaiwala <zoebmithaiwala@yahoo.com>
2026-09-15 15:30:05 +0530

    Remove old site content
    Clearing the way for a full rebuild of the personal site.

--- blog/int2Text.py
+++
@@ -1,9 +0,0 @@
-def int2Text(number, size):
-    text = "".join([chr((number >> j) & 0xff)
-                    for j in reversed(range(0, size << 3, 8))])
-    return text.lstrip("\x00")
-
-num = 555555555 #The integer to be converted to string
-s = 513
-print num,s
-print int2Text(num,s)
